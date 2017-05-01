7783af62b651ab7c64f75712b9bf87220e6c04fe
zssazrael <zsszbox@qq.com>
2017-05-01 12:55:51 +0800

    commit

--- src/main/java/zss/tool/web/WebTool.java
+++ src/main/java/zss/tool/web/WebTool.java
@@ -15,47 +15,41 @@ import org.slf4j.LoggerFactory;
 
 import zss.tool.LoggedException;
 import zss.tool.ReflectTool;
+import zss.tool.StringObjectMap;
 import zss.tool.Version;
 
-@Version("2016-09-05")
-public final class WebTool
-{
+@Version("2017.05.01")
+public final class WebTool {
     private static final Logger LOGGER = LoggerFactory.getLogger(WebTool.class);
 
     private static final int DEFAULT_HTTP_PORT = 80;
     private static final int DEFAULT_HTTPS_PORT = 443;
 
-    private WebTool()
-    {
+    private WebTool() {
     }
 
-    public static String getRequestURI(final HttpServletRequest request)
-    {
+    public static String getRequestURI(final HttpServletRequest request) {
         return request.getRequestURI().substring(request.getContextPath().length());
     }
 
-    public static String getRequestURL(final HttpServletRequest request)
-    {
+    public static String getRequestURL(final HttpServletRequest request) {
         StringBuffer buffer = request.getRequestURL();
         String query = request.getQueryString();
-        if (StringUtils.isNotEmpty(query))
-        {
+        if (StringUtils.isNotEmpty(query)) {
             buffer.append('?');
             buffer.append(query);
         }
         return buffer.toString();
     }
 
-    public static String getContextURL(final HttpServletRequest request, final String path)
-    {
+    public static String getContextURL(final HttpServletRequest request, final String path) {
         StringBuilder builder = new StringBuilder();
         String scheme = request.getScheme();
         builder.append(scheme);
         builder.append("://");
         builder.append(request.getServerName());
         int port = request.getServerPort();
-        if (("http".equals(scheme) && (port != DEFAULT_HTTP_PORT)) || ("https".equals(scheme) && (port != DEFAULT_HTTPS_PORT)))
-        {
+        if (("http".equals(scheme) && (port != DEFAULT_HTTP_PORT)) || ("https".equals(scheme) && (port != DEFAULT_HTTPS_PORT))) {
             builder.append(":");
             builder.append(Integer.toString(port, 10));
         }
@@ -64,61 +58,55 @@ public final class WebTool
         return builder.toString();
     }
 
-    public static String getContextURL(final HttpServletRequest request)
-    {
+    public static String getContextURL(final HttpServletRequest request) {
         return getContextURL(request, "/");
     }
 
-    public static <T> T getAttribute(final ServletRequest request, final String name, final Class<T> type)
-    {
+    public static <T> T getAttribute(final ServletRequest request, final String name, final Class<T> type) {
         return ReflectTool.cast(request.getAttribute(name), type);
     }
 
-    public static <T> T getAttribute(final HttpSession session, final String name, final Class<T> type)
-    {
+    public static <T> T getAttribute(final HttpSession session, final String name, final Class<T> type) {
         return ReflectTool.cast(session.getAttribute(name), type);
     }
 
-    public static void forward(final ServletRequest request, final ServletResponse response, final String path)
-    {
-        try
-        {
+    public static void forward(final ServletRequest request, final ServletResponse response, final String path) {
+        try {
             request.getRequestDispatcher(path).forward(request, response);
-        }
-        catch (ServletException e)
-        {
+        } catch (ServletException e) {
             LOGGER.error(e.getMessage(), e);
             throw new LoggedException();
-        }
-        catch (IOException e)
-        {
+        } catch (IOException e) {
             LOGGER.error(e.getMessage(), e);
             throw new LoggedException();
         }
     }
 
-    public static Cookie getCookie(final HttpServletRequest request, final String name)
-    {
-        if ((request == null) || (name == null))
-        {
+    public static Cookie getCookie(final HttpServletRequest request, final String name) {
+        if ((request == null) || (name == null)) {
             return null;
         }
         return getCookie(request.getCookies(), name);
     }
 
-    public static Cookie getCookie(final Cookie[] cookies, final String name)
-    {
-        if ((cookies == null) || (name == null))
-        {
+    public static Cookie getCookie(final Cookie[] cookies, final String name) {
+        if ((cookies == null) || (name == null)) {
             return null;
         }
-        for (Cookie cookie : cookies)
-        {
-            if (name.equals(cookie.getName()))
-            {
+        for (Cookie cookie : cookies) {
+            if (name.equals(cookie.getName())) {
                 return cookie;
             }
         }
         return null;
+    }
+
+    public static StringObjectMap getModelMap(final HttpServletRequest request) {
+        Object object = request.getAttribute("model");
+        if (!(object instanceof StringObjectMap)) {
+            object = new StringObjectMap();
+            request.setAttribute("model", object);
+        }
+        return (StringObjectMap) object;
     }
 }
