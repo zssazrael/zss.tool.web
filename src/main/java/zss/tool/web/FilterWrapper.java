9a23e5befa891e5a5480bd6a6f54dbfbc3bbaeca
zssazrael <zsszbox@qq.com>
2017-07-21 14:09:55 +0800

    commit

---
+++ src/main/java/zss/tool/web/FilterWrapper.java
@@ -0,0 +1,58 @@
+package zss.tool.web;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+import zss.tool.Version;
+
+@Version("2017.07.21")
+public class FilterWrapper implements Filter {
+    private FilterConfig filterConfig;
+    private Filter filter;
+
+    public Filter getFilter() {
+        return filter;
+    }
+
+    public FilterConfig getFilterConfig() {
+        return filterConfig;
+    }
+
+    public void setFilter(Filter filter) throws ServletException {
+        if (this.filter != null) {
+            this.filter.destroy();
+        }
+        this.filter = filter;
+        if (filter != null) {
+            filter.init(filterConfig);
+        }
+    }
+
+    @Override
+    public void init(final FilterConfig filterConfig) throws ServletException {
+        this.filterConfig = filterConfig;
+        filterConfig.getServletContext().setAttribute(filterConfig.getFilterName(), this);
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        if (filter == null) {
+            chain.doFilter(request, response);
+        } else {
+            filter.doFilter(request, response, chain);
+        }
+    }
+
+    @Override
+    public void destroy() {
+        if (filter != null) {
+            filter.destroy();
+        }
+    }
+}
